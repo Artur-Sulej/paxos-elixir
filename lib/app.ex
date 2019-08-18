f8f48e6a7de46a22ad2eb3a472e7ca5e216e056f
Artur Sulej <artur@shedul.com>
2019-08-18 18:57:34 +0200

    Building children to start

--- lib/app.ex
+++ lib/app.ex
@@ -2,25 +2,37 @@ defmodule Paxos.App do
   use Application
 
   def start(_type, _args) do
-    node_id = System.get_env("NODE_ID")
+    registration_node = System.get_env("REG_NODE")
+    proposer_id = System.get_env("PROPOSER_ID")
+    acceptor_id = System.get_env("ACCEPTOR_ID")
+    learner_id = System.get_env("LEARNER_ID")
 
-    proposer = System.get_env("PROPOSER") && Paxos.Proposer
-    acceptor = System.get_env("ACCEPTOR") && Paxos.Acceptor
-    learner = System.get_env("LEARNER") && Paxos.Learner
-    children = Enum.reject([proposer, acceptor, learner], &is_nil/1)
+    connect_node(registration_node)
+    proposer_id && Paxos.Registration.register_proposer(proposer_id)
+    acceptor_id && Paxos.Registration.register_acceptor(acceptor_id)
+    learner_id && Paxos.Registration.register_learner(learner_id)
+
+    children =
+      Enum.reject(
+        [
+          proposer_id && Paxos.Proposer,
+          acceptor_id && Paxos.Acceptor,
+          learner_id && Paxos.Learner
+        ],
+        &is_nil/1
+      )
 
     opts = [strategy: :one_for_one, name: Paxos.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
-  defp register_name(registration_node, server_name) do
-    Node.start(String.to_atom(server_name), :shortnames)
-    Node.connect(String.to_atom(registration_node))
-    :global.sync()
-    Paxos.Registration.add_own_name(server_name)
+  defp generate_server_name do
+    :crypto.strong_rand_bytes(15) |> Base.url_encode64()
   end
 
-  defp generate_server_name do
-    System.get_env("SERVER_NAME") || :crypto.strong_rand_bytes(15) |> Base.url_encode64()
+  defp connect_node(registration_node) do
+    Node.start(String.to_atom(generate_server_name), :shortnames)
+    Node.connect(String.to_atom(registration_node))
+    :global.sync()
   end
 end
