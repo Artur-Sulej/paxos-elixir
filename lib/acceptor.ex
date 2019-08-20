350392da67c8b40d41e77b4c5850232d0b569e98
Artur Sulej <artur@shedul.com>
2019-08-20 22:31:09 +0200

    Handling prepare_request message

--- lib/acceptor.ex
+++ lib/acceptor.ex
@@ -4,7 +4,7 @@ defmodule Paxos.Acceptor do
   ### External API
 
   def start_link(acceptor_id) do
-    {:ok, pid} = GenServer.start_link(__MODULE__, [], name: __MODULE__)
+    {:ok, pid} = GenServer.start_link(__MODULE__, nil, name: __MODULE__)
 
     name = build_name(acceptor_id)
     :global.register_name(name, pid)
@@ -15,13 +15,26 @@ defmodule Paxos.Acceptor do
 
   ### GenServer implementation
 
-  def init(args) do
-    {:ok, args}
+  def init(_args) do
+    {:ok, %{accepted: [], proposed: nil}}
   end
 
-  def handle_call(%{id: id, value: value, type: :prepare_request}, _, current_val) do
-    IO.puts("---- handle_call #{inspect(%{id: id, value: value, type: :prepare_request})} ---")
-    {:reply, current_val, current_val}
+  def handle_call(
+        %{id: id, value: value, type: :prepare_request},
+        _from,
+        current_state = %{proposed: prev_proposed}
+      ) do
+    IO.puts("---- #{inspect(%{id: id, value: value, type: :prepare_request})} ---")
+
+    {reply, new_state} =
+      if prev_proposed == nil || prev_proposed.id < id do
+        {%{id: id, previous: nil, type: :prepare_response},
+         Map.put(current_state, :proposed, %{id: id, value: value})}
+      else
+        {%{id: id, previous: prev_proposed, type: :prepare_response}, current_state}
+      end
+
+    {:reply, reply, new_state}
   end
 
   def handle_call(:get_state, _from, current_state) do
